6264e41f031053a300158cb46eef8d5b317ad75a
Joern Bernhardt <joern.bernhardt@compose.us>
2022-06-17 13:03:48 +0200

    fix: Add slash for sed
    Signed-off-by: Joern Bernhardt <joern.bernhardt@compose.us>

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mkdir build/
 
 cp src/CNAME build
 
-cat src/index.html | sed "s/LAST_UPDATED_AT/$(date)/;s/GREETING/$NAME" > build/index.html
+cat src/index.html | sed "s/LAST_UPDATED_AT/$(date)/;s/GREETING/$NAME/" > build/index.html
